769b2a1e7418311f4eccc56acf668b8c8113a62a
Nicolas França <nicolasfranca9@gmail.com>
2015-12-06 02:29:58 +0000

    Faça um programa que peça dois numeros e imprima a soma.

---
+++ EstruturaSequencial/ExercicioTres.php
@@ -0,0 +1,11 @@
+<?php
+
+// Faça um Programa que peça dois números e imprima a soma. 
+
+function sumTwoNumbers($a, $b) {
+    $sum = $a + $b;
+    
+    echo "{$sum}\n";
+}
+
+sumTwoNumbers(5, 9); // => 14
