aac9f618478fd4e0594858b86e678f31521dc53d
Nicolas França <nicolasfranca9@gmail.com>
2015-12-06 02:29:20 +0000

    Faça um programa que peça um numero ...

---
+++ EstruturaSequencial/ExercicioDois.php
@@ -0,0 +1,9 @@
+<?php
+
+// Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]. 
+
+function getAnNumber($number) {
+    echo "O número informado foi {$number}\n";
+}
+
+getAnNumber(5);
